4e47014592294f134c64b96a1eb2dc7d41f938e7
Hizkia Siahaan <siahaanhizkia548@gmail.com>
2025-06-25 18:58:33 +0700

    none

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -87,7 +87,7 @@ const Navbar = () => {
                     </div>
                 </div>
             </div>
-            <div className={`fixed top-0 right-0 h-full w-80 bg-white shadow-2xl transform transition-transform duration-300 ease-in-out z-50 md:hidden flex flex-col ${
+            <div className={`fixed top-0 right-0 h-full w-70 bg-white shadow-2xl transform transition-transform duration-300 ease-in-out z-50 md:hidden flex flex-col ${
                 isMobileMenuOpen ? 'translate-x-0' : 'translate-x-full'
             }`}>
                 <div className="flex justify-between items-center p-6 border-b border-gray-200">
