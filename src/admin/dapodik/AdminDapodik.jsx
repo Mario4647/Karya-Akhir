9663bc046a7fc921d290f1c08a5764b9018f0adf
Its me Kazuyaa. <99629860+Mario4647@users.noreply.github.com>
2025-12-06 13:56:50 +0700

    Update AdminDapodik.jsx

--- src/admin/dapodik/AdminDapodik.jsx
+++ src/admin/dapodik/AdminDapodik.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { supabase, supabaseAdmin, uploadRaportPDF } from '../supabaseClient';
+import { supabase, supabaseAdmin, uploadRaportPDF } from '../../supabaseClient';
 
 const AdminDapodik = () => {
   const [students, setStudents] = useState([]);
